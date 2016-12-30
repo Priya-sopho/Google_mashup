a3499959578b2c48f14f6b494e6a3237dbc406e8
Priya <prrani28@gmail.com>
2016-12-30 21:49:47 +0530

    updated search controller via adding sql query

--- html/search.php
+++ html/search.php
@@ -5,7 +5,14 @@
     // numerically indexed array of places
     $places = [];
 
-    // TODO: search database for places matching $_GET["geo"], store in $places
+    //fulltext search for geo field's value on joint key in database
+    $rows = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name, admin_name1, admin_name2) AGAINST (?)",
+            $_GET["geo"]);
+    foreach($rows as $row)
+    {
+       //add each row to places
+       array_push($places, $row);
+    }            
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
